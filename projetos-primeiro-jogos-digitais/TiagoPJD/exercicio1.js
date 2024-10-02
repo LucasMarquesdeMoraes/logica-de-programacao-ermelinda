b65c7a10fdaabddfae29f98622866571b91706a8
Tiago <tiagodeoliveiracoelho4@gmail.com>
2024-10-02 13:23:21 -0300

    Add files via upload

---
+++ projetos-primeiro-jogos-digitais/TiagoPJD/exercicio1.js
@@ -0,0 +1,310 @@
+/*Leia o número de passageiros adultos e crianças.
+Solicite que o usuário insira a região de destino (Norte ou Sul do Brasil) ou o continente (Ásia, Europa, África) e a classe de voo (Econômica ou Executiva).
+Calcule o custo total da viagem com base no número de passageiros, no destino e na classe escolhida.
+Exiba o custo total da viagem e todas as informações sobre: a quantidade de passageiros adultos, infantil, qual a classe do voo, e o destino.
+Comente em cada linha do seu código para documentá-lo.
+Desenvolva o fluxograma do projeto para ilustrar a lógica do programa.
+Crie o design no Figma ou em outro aplicativo para demonstrar o esboço da interface estática.
+Entregue o projeto em um único arquivo
+ 
+ 
+/*Voo para o norte Nacional executivo adultos e crianças*/
+ 
+let passagemadult =2;
+let voonacional= "Norte";
+let classedesejada = "Executiva";
+let precoadult =2500.00 * 1.20;
+let passagemcria =1;
+let preccria =2200 * 1.10;
+let numdepassageiros= passagemadult+passagemcria;
+ 
+if (voonacional="Norte") {
+    console.log("Seu voo é:", voonacional);
+    if(classedesejada="Executiva") {
+    console.log("E seu voo é de classe:", classedesejada);
+    }
+    if (precoadult=2500.00 * 1.20) {
+    console.log("Preço da passagens adultos:", precoadult);  
+    }
+    if (passagemadult =2) {
+    console.log("número de passagens de adultos:", passagemadult);    
+    }
+    if (preccria=2200 * 1.10) {
+    console.log("Preço das passagens infantis", preccria);
+    }
+    if (numdepassageiros=passagemadult+passagemcria) {
+    console.log("Respeite os demais passageiros, temos hoje nesse voo:", numdepassageiros);
+ 
+    console.log("________________________________________________________________________________________________");
+    console.log("________________________________________________________________________________________________");
+    console.log("                                                                                                ");
+    }
+   
+}
+/*Voo para o norte Nacional econômica adultos e crianças*/
+let passagemadulto =15;
+let voonacionall= "Norte";
+let classedesejadaa = "Econômica";
+let precoadulto =1200.00;
+let passagemcrian =5;
+let preccrian =900;
+let numdepassageiross= passagemadulto+passagemcrian;
+ 
+if (voonacionall="Norte") {
+    console.log("Seu voo é:", voonacionall);
+    if(classedesejada="Econômica") {
+    console.log("E seu voo é de classe:", classedesejadaa);
+    }
+    if (precoadult=1200.00) {
+    console.log("Preço da passagens adultos:", precoadulto);  
+    }
+    if (passagemadult =2) {
+    console.log("número de passagens de adultos:", passagemadulto);    
+    }
+    if (preccria=900) {
+    console.log("Preço das passagens infantis", preccrian);
+    }
+    if (numdepassageiross=passagemadulto+passagemcrian) {
+    console.log("Respeite os demais passageiros, temos hoje nesse voo:", numdepassageiross);
+    }
+}
+ 
+console.log("________________________________________________________________________________________________");
+console.log("________________________________________________________________________________________________");
+console.log("                                                                                                ");
+ 
+/*Voo nacional para o sul, classe econômica adultos e crianças*/
+let passgemadlto =15;
+let vooncinall= "Sul";
+let classedesjdaa = "Econômica";
+let precodlto =2200.00;
+let passgemcran =5;
+let preccran =2200.00;
+let numdepasageirss= passgemadlto+passgemcran;
+ 
+if (voonacinall="Sul") {
+    console.log("Seu voo é:", voonacinall);
+    if(classedesejada="Econômica") {
+    console.log("E seu voo é de classe:", classedesejadaa);
+    }
+    if (precoadlt=2200.00) {
+    console.log("Preço da passagens adultos:", precodlto);  
+    }
+    if (passgemadulto =2) {
+    console.log("número de passagens de adultos:", passgemadlto);    
+    }
+    if (preccria=2200.00) {
+    console.log("Preço das passagens infantis", preccran);
+    }
+    if (numdepasageirss=passgemadlto+passgemcran) {
+    console.log("Respeite os demais passageiros, temos hoje nesse voo:", numdepasageirss);
+    }
+}
+console.log("________________________________________________________________________________________________");
+console.log("________________________________________________________________________________________________");
+console.log("                                                                                                ");
+ 
+/*Voo nacional para o sul, classe econômica adultos e crianças*/
+let pasagmadult =2;
+let voonaional= "Sul";
+let clasedesejada = "Executiva";
+let prcoadult =3400.00;
+let pasagmcria =1;
+let prccria =3400.00;
+let numdepasageiros= pasagmadult+pasagmcria;
+ 
+if (voonaional="Sul") {
+    console.log("Seu voo é:", voonaional);
+    if(clasedesejada="Executiva") {
+    console.log("E seu voo é de classe:", clasedesejada);
+    }
+    if (precoadult=3400.00) {
+    console.log("Preço da passagens adultos:", precoadult);  
+    }
+    if (pasagmadult =2) {
+    console.log("número de passagens de adultos:", pasagmadult);    
+    }
+    if (prccria=3400.00) {
+    console.log("Preço das passagens infantis", prccria);
+    }
+    if (numdepasageiros=pasagmadult+pasagmcria) {
+    console.log("Respeite os demais passageiros, temos hoje nesse voo:", numdepasageiros);
+    }
+}
+console.log("________________________________________________________________________________________________");
+console.log("________________________________________________________________________________________________");
+console.log("                                                                                                ");
+ 
+/*Voo internacional para a Ásia executivo adultos e crianças*/
+let passagemadul =2;
+let voointernacional= "Ásia";
+let classedesejad = "Executiva";
+let precoadultt =5000.00;
+let passagemcri =1;
+let preccri =5000.00;
+let numdepassageir= passagemadul+passagemcri;
+ 
+if (voointernacional="Ásia") {
+    console.log("Seu voo é para:", voointernacional);
+    if(classedesejad="Executiva") {
+    console.log("E seu voo é de classe:", classedesejad);
+    }
+    if (precoadultt=5000.00) {
+    console.log("Preço da passagens adultos:", precoadultt);  
+    }
+    if (passagemadul =2) {
+    console.log("número de passagens de adultos:", passagemadul);    
+    }
+    if (preccria=5000.00) {
+    console.log("Preço das passagens infantis", preccri);
+    }
+    if (numdepassageiros=passagemadul+passagemcri) {
+    console.log("Respeite os demais passageiros, temos hoje nesse voo:", numdepassageir);
+    }
+}
+console.log("________________________________________________________________________________________________");
+console.log("________________________________________________________________________________________________");
+console.log("                                                                                                ");
+ 
+/*Voo internacional para a Ásia Econômica adultos e crianças*/
+let passagemadu =2;
+let voointernaciona= "Ásia";
+let classedeseja = "Econômica";
+let precoadul =3600.00;
+let passagemcr =1;
+let preccr =3600.00;
+let numdepassagei= passagemadu+passagemcr;
+ 
+if (voointernaciona="Ásia") {
+    console.log("Seu voo é para:", voointernaciona);
+    if(classedeseja="Econômica") {
+    console.log("E seu voo é de classe:", classedeseja);
+    }
+    if (precoadul=3600.00) {
+    console.log("Preço da passagens adultos:", precoadul);  
+    }
+    if (passagemadu =2) {
+    console.log("número de passagens de adultos:", passagemadu);    
+    }
+    if (preccria=3600.00) {
+    console.log("Preço das passagens infantis", preccri);
+    }
+    if (numdepassagei=passagemadu+passagemcri) {
+    console.log("Respeite os demais passageiros, temos hoje nesse voo:", numdepassagei);
+    }
+}
+console.log("________________________________________________________________________________________________");
+console.log("________________________________________________________________________________________________");
+console.log("                                                                                                ");
+ 
+/*Voo internacional para a Ásia executivo adultos e crianças*/
+let passagemadl =2;
+let voointernacionl= "Europa";
+let classedesejd = "Executiva";
+let precoadltt =7.300;
+let passagecri =1;
+let prccri =7.300;
+let numdepasageir= passagemadl+passagecri;
+ 
+if (voointernacionl="Europa") {
+    console.log("Seu voo é para:", voointernacionl);
+    if(classedesejd="Executiva") {
+    console.log("E seu voo é de classe:", classedesejd);
+    }
+    if (precoadultt=7.300) {
+    console.log("Preço da passagens adultos:", precoadultt);  
+    }
+    if (passagemadl =2) {
+    console.log("número de passagens de adultos:", passagemadl);    
+    }
+    if (preccria=7.300) {
+    console.log("Preço das passagens infantis", preccri);
+    }
+    if (numdepassageir=passagemadl+passagecri) {
+    console.log("Respeite os demais passageiros, temos hoje nesse voo:", numdepassageir);
+    }
+}
+/*Voo internacional para a Ásia econômico adultos e crianças*/
+let pasagemadl =2;
+let vointernacionl= "Europa";
+let clasedesejd = "Econômica";
+let precoaltt =5.800;
+let pasagecri =1;
+let pccri =5.800;
+let nudepasageir= pasagemadl+pasagecri;
+ 
+if (vointernacionl="Europa") {
+    console.log("Seu voo é para:", vointernacionl);
+    if(classedesejd="Econômica") {
+    console.log("E seu voo é de classe:", clasedesejd);
+    }
+    if (precoaltt=5.800) {
+    console.log("Preço da passagens adultos:", precoaltt);  
+    }
+    if (pasagemadl =2) {
+    console.log("número de passagens de adultos:", pasagemadl);    
+    }
+    if (preccria=5.800) {
+    console.log("Preço das passagens infantis", preccri);
+    }
+    if (nudepassageir=pasagemadl+pasagecri) {
+    console.log("Respeite os demais passageiros, temos hoje nesse voo:", nudepassageir);
+    }
+}
+console.log("________________________________________________________________________________________________");
+console.log("________________________________________________________________________________________________");
+console.log("                                                                                                ");
+/*Voo internacional para a África econômico adultos e crianças*/
+let pasagemal =2;
+let vointernaconl= "África";
+let clasedesjd = "Econômica";
+let precoalt =2200.00;
+let pasagecr =1;
+let pccr =2200.00;
+let nudepasagei= pasagemal+pasagecr;
+ 
+if (vointernaconl="África") {
+    console.log("Seu voo é para:", vointernaconl);
+    if(classedesejd="Econômica") {
+    console.log("E seu voo é de classe:", clasedesjd);
+    }
+    if (precoalt=2200.00) {
+    console.log("Preço da passagens adultos:", precoalt);  
+    }
+    if (pasagemadl =2) {
+    console.log("número de passagens de adultos:", pasagemal);    
+    }
+    if (preccria=2200.00) {
+    console.log("Preço das passagens infantis", pccr);
+    }
+    if (nudepassagei=pasagemal+pasagecr) {
+    console.log("Respeite os demais passageiros, temos hoje nesse voo:", nudepassagei);
+    }
+}
+/*Voo internacional para a Ásia Executivo adultos e crianças*/
+let pasgemadtl =2;
+let iternacionl= "África";
+let clasedesej = "Executivo";
+let precoadtl =3900.00;
+let pasagecriii =1;
+let pccriii =3900.00;
+let nudepasageiroo= pasgemadtl+pasagecriii;
+ 
+if (internacionl="África") {
+    console.log("Seu voo é para:", internacionl);
+    if(classedesejd="Executivo") {
+    console.log("E seu voo é de classe:", clasedesejd);
+    }
+    if (precoaltt=3900.00) {
+    console.log("Preço da passagens adultos:", precoadtl);  
+    }
+    if (pasagemadtl =2) {
+    console.log("número de passagens de adultos:", pasagemadtl);    
+    }
+    if (preccria=3900.00) {
+    console.log("Preço das passagens infantis", preccri);
+    }
+    if (nudepassageiroo=pasagemadtl+pasagecriii) {
+    console.log("Respeite os demais passageiros, temos hoje nesse voo:", nudepassageiroo);
+    }
+}
